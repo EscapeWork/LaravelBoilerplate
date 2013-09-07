7fa0601e84a0776d6a1fe35a0e8ecd7dec919a7a
Luis Dalmolin <luis.nh@gmail.com>
2013-09-07 10:39:41 -0300

    Fixed Escape logo

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -3,5 +3,5 @@
 @section('content')
     <h1>Hello from Escape Criativação</h1>
 
-    <img width="500" src="https://s3.amazonaws.com/escape-criativacao/labs/logo-escape-svg/escape-criativacao.svg?AWSAccessKeyId=ASIAJWMEWGOBACZ4Z5UQ&Expires=1377883009&Signature=KZ5AxdvUu5fibEeid9dX4%2B6OwaA%3D&x-amz-security-token=AQoDYXdzEJL//////////wEawALtLekHrqQwSMVThcKG889csxvQ5CIV6Muvw4bl%2BLbo8Dg/8yv7KnL/dJzIODflo6/mGCYo/oNA5GT9HgHDlu1cxk0Fo90fxJPLvz4PTK5rPZrn8DF1bNXkQ%2BASCzE3RApG3bWVA/pPbbGBgjsK7Mj0%2BY0PD8qvCu6aSZTqY7mY9l7LCc/zuFOKbjUX6fqbsqM8iGmv6Fkv4Wj5LE5jKyTaqdpxUML8VcSr2i/aEuT3CuG3x3xEg/VmqjXEsoNWHJOk5VvJtcvkpwofWSVom48xb83NW4CvP2edk%2B0KYIQ3m3akkkOuBtqNKtmouX9/TmSyuE5wT9bQcHCYv25Yt31TwtMveHSsc2eykLFLSJcTjGfOfOKof5erZcwpGTsgfG6ZVpZ8lR2CFQT8UsWjH%2BUZtXv53tjhVI1bz4CbxlUWkiDJo4ORBQ%3D%3D" alt="" />
+    <img width="500" src="https://s3.amazonaws.com/escape-criativacao/labs/logo-escape-svg/escape-criativacao.svg" alt="" />
 @stop
