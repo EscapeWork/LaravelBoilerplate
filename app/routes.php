8a35bc772e4f6fff1dbe8031c02b099c5252e5e7
Luis Dalmolin <luis.nh@gmail.com>
2013-09-07 10:43:11 -0300

    Little change in routes.php file

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,16 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
 
-Route::get('/', function()
+Route::get('/', array('as' => 'home', function()
 {
-    return View::make('home/home');
-});
+    return View::make('home.home');
+}));
