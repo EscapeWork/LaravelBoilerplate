c31cfa4b2059bca5c331670b469872257440ece2
Luis Dalmolin <luis.nh@gmail.com>
2014-05-21 14:07:33 -0300

    Adicionado fallback_locale no app.php - Adicionado arquivos de linguas em pt-br

--- app/config/app.php
+++ app/config/app.php
@@ -54,6 +54,19 @@ return array(
 
 	'locale' => 'pt-br',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Fallback Locale
+    |--------------------------------------------------------------------------
+    |
+    | The fallback locale determines the locale to use when the current one
+    | is not available. You may change the value to correspond to any of
+    | the language folders that are provided through your application.
+    |
+    */
+
+    'fallback_locale' => 'pt-br',
+
 	/*
 	|--------------------------------------------------------------------------
 	| Encryption Key
---
+++ app/lang/pt-br/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "As senhas precisam ter no mínimo 6 caracteres e elas precisam ser iguais.",
+
+    "user"     => "Não conseguimos encontrar nenhum usuário ativo com este email.",
+
+    "token"    => "O token é inválido.",
+
+    "sent"     => "Lembrete de senha enviado!",
+
+);
