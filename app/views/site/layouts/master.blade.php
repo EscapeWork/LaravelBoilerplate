463c6bc0c9d5a45fdd056b3ed5f64808fcb1b0b0
Luis Dalmolin <luis.nh@gmail.com>
2014-07-21 15:17:17 -0300

    Added some new tags in the master blade layout

--- app/views/site/layouts/master.blade.php
+++ app/views/site/layouts/master.blade.php
@@ -1,15 +1,21 @@
 <!doctype html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     @section('seo')
         <title>Laravel Bootstrap</title>
-        <link rel="canonical" href="{{ route('home') }}" />
+        <meta name="description" content="">
+        <link rel="canonical" href="{{ route('home') }}">
     @show
 
+    <!-- favicon -->
+    <link rel="shotcurt icon" href="{{ asset('assets/images/favicon.ico') }}" text="image/x-icon">
+
     @section('styles')
-        <link rel="stylesheet" type="text/css" href="{{ Asset::v('assets/stylesheets/css/main.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ Asset::v('assets/stylesheets/css/main.css') }}">
     @show
 
     <script type="text/javascript" src="{{ asset('assets/components/modernizr/modernizr.js') }}"></script>
