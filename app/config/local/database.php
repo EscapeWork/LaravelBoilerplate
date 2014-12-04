13c42bc4bc55e5a8a69061a746c8f7d094346831
Luis Dalmolin <luis.nh@gmail.com>
2014-12-04 15:52:38 -0200

    Added some template strings to local/database.php config file

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => '',
-            'database'  => '',
-            'username'  => '',
-            'password'  => '',
+            'host'      => '#host#',
+            'database'  => '#name#',
+            'username'  => '#user#',
+            'password'  => '#pass#',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
