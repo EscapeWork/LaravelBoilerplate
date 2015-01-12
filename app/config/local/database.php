1c65f0ba822fc7e9cb9b82fe32dde49b4ba6f1e9
Luis Dalmolin <luis.nh@gmail.com>
2015-01-12 14:07:59 -0200

    Fixed local/database.php config

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -54,10 +54,11 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => #host#,
-            'database'  => #name#,
-            'username'  => #user#,
-            'password'  => #pass#,
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USER'),
+            'password'  => getenv('DB_PASS'),
+            'port'      => getenv('DB_PORT') ?: 3306,
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
