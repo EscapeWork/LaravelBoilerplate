1ff4dd8033cee8ead163808e126bd5a46de8252b
Luis Dalmolin <luis.nh@gmail.com>
2015-01-12 10:39:23 -0200

    Fixed local/database.php config file

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -54,10 +54,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => '#host#',
-            'database'  => '#name#',
-            'username'  => '#user#',
-            'password'  => '#pass#',
+            'host'      => #host#,
+            'database'  => #name#,
+            'username'  => #user#,
+            'password'  => #pass#,
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
