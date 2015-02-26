281fe3b7efdfbba8602c3f5317de0dbd95f7f5ba
Luis Dalmolin <luis.nh@gmail.com>
2015-02-26 08:14:47 -0300

    Added te language files in pt-br

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -73,6 +73,7 @@ return [
 	"unique"               => "The :attribute has already been taken.",
 	"url"                  => "The :attribute format is invalid.",
 	"timezone"             => "The :attribute must be a valid zone.",
+    "image_array"          => "Please, send only images",
 
 	/*
 	|--------------------------------------------------------------------------
