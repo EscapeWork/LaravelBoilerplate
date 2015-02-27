3b15039bf78a89831a866aa48dd208308d1480ee
Luis Dalmolin <luis.nh@gmail.com>
2015-02-27 13:08:21 -0300

    Added a little ajax helper for error messages

--- resources/assets/javascript/app/main.js
+++ resources/assets/javascript/app/main.js
@@ -1,3 +1,17 @@
 (function() {
     window.App = window.App || {};
+
+    // this work with laravel form requests responses
+    // usage: App.Messages.show(messages)
+    App.Messages = {
+        show: function(messages) {
+            var msg = [];
+
+            $.each(messages, function(i, message) {
+                msg.push(message[0]);
+            });
+
+            alert(msg.join("\n"));
+        }
+    };
 })();
