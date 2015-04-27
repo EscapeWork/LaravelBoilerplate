7562a3d51d75eb6398b05eacc5c39e3df445200b
Luis Dalmolin <luis.nh@gmail.com>
2015-04-27 09:49:07 -0300

    Changed the contact class

--- resources/assets/javascript/app/contact.js
+++ resources/assets/javascript/app/contact.js
@@ -1,63 +1,8 @@
 (function() {
     window.App = window.App || {};
 
-    // Contact
-    App.Contact = {
-        init: function() {
-            this.fetchElements();
-            this.bindUIEvents();
-        },
-
-        fetchElements: function() {
-            this.$form = $('.js-contact-form');
-            this.$btn  = this.$form.find('.js-btn-submit');
-
-            this.$form.find('input[name="phone"]').inputmask('(99) 9999.99999');
-        },
-
-        bindUIEvents: function() {
-            this.$form.on('submit', $.proxy(this._onSubmit, this));
-        },
-
-        _onSubmit: function(e) {
-            e.preventDefault();
-
-            this.send();
-        },
-
-        send: function() {
-            var _this = this;
-
-            $.ajax({
-                type: 'POST',
-                url: _this.$form.prop('action'),
-                data: _this.$form.serialize(),
-
-                beforeSend: function() {
-                    _this.$btn.prop('disabled', true);
-                }
-            })
-                .success(function(data) {
-                    alert(data.message);
-
-                    _this.clearForm();
-
-                    // google analytics (uncomment this if installed)
-                    // ga('send', 'event', 'Contato', 'Enviado');
-                })
-                .error(function(data) {
-                    App.Messages.show(data.responseJSON);
-                })
-                .always(function() {
-                    _this.$btn.prop('disabled', false);
-                });
-        },
-
-        clearForm: function() {
-            this.$form.find('.input').val('');
-        }
-    };
-
-    // inits
-    App.Contact.init();
+    App.ContactForm = new App.Contact('.js-contact-form', {
+        conversionCategory: 'Contato',
+        conversionAction: 'Enviado'
+    });
 })();
--- resources/assets/javascript/app/main.js
+++ resources/assets/javascript/app/main.js
@@ -14,4 +14,67 @@
             alert(msg.join("\n"));
         }
     };
+
+    // contact class
+    App.Contact = function(form, options) {
+        this.fetchElements = function() {
+            this.$form   = $(form);
+            this.$btn    = this.$form.find('.js-btn-submit');
+            this.options = $.extend({conversionLabel: ''}, options);
+
+            this.$form.find('input[type="tel"]').inputmask('(99) 9999.99999');
+        };
+
+        this.bindUIEvents = function() {
+            this.$form.on('submit', $.proxy(this._onSubmit, this));
+        };
+
+        this._onSubmit = function(e) {
+            e.preventDefault();
+
+            this.send();
+        };
+
+        this.send = function() {
+            var _this = this;
+
+            $.ajax({
+                type: 'POST',
+                url: _this.$form.prop('action'),
+                data: _this.$form.serialize(),
+
+                beforeSend: function() {
+                    _this.$btn.prop('disabled', true);
+                }
+            })
+                .success(function(data) {
+                    alert(data.message);
+
+                    _this.clearForm();
+
+                    // google analytics
+                    ga(
+                        'send',
+                        'event',
+                        _this.options.conversionCategory,
+                        _this.options.conversionAction,
+                        _this.options.conversionLabel
+                    );
+                })
+                .error(function(data) {
+                    App.Messages.show(data.responseJSON);
+                })
+                .always(function() {
+                    _this.$btn.prop('disabled', false);
+                });
+        };
+
+        this.clearForm = function() {
+            this.$form.find('.input').val('');
+        };
+
+        // inits
+        this.fetchElements();
+        this.bindUIEvents();
+    };
 })();
