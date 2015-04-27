fa5aecd284990f9451308292ddd1bc8259159dd0
Luis Dalmolin <luis.nh@gmail.com>
2015-04-27 20:09:22 -0300

    Fixed error views

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('app.master')
+
+@section('content')
+    <section class="container">
+        <h1 class="content-title">Página não encontrada</h1>
+
+        <div class="content-text">
+            <p>Desculpe, mas não conseguimos encontrar o que voê está procurando.</p>
+        </div>
+    </section>
+@stop
---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('app.master')
+
+@section('content')
+    <section class="container">
+        <h1 class="content-title">Já voltamos!</h1>
+
+        <div class="content-text">
+            <p>Estamos realizando uma manutenção em nosso site e já vamos voltar.</p>
+        </div>
+    </section>
+@stop
