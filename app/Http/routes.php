d380f465cb5e7b3069e553610ef559e3e77bdc11
Luis Dalmolin <luis.nh@gmail.com>
2015-06-17 12:27:02 -0300

    Added 'as' prefix in the manager group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,7 +17,7 @@ Route::post('contato', ['as' => 'contact.send', 'uses' => 'App\ContactController
 
 
 # manager
-Route::group(['prefix' => config('manager.url'), 'middleware' => 'manager.authenticated'], function()
+Route::group(['prefix' => config('manager.url'), 'middleware' => 'manager.authenticated', 'as' => 'manager.'], function()
 {
     # if you do not use escapework/manager (private package) - just remove this group
 });
