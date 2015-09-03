30e68716731941889d4f9629e4c014c182870b00
Luis Dalmolin <luis.nh@gmail.com>
2015-09-03 14:15:27 -0300

    Changed the default password view content

--- resources/views/emails/password.blade.php
+++ resources/views/emails/password.blade.php
@@ -1,5 +1,13 @@
 @extends('emails.master')
 
 @section('content')
-    Click here to reset your password: {{ url('password/reset/'.$token) }}
+    <p>Olá, </p>
+
+    <p>Para refazer sua senha no site da SEEAATESP, acesse o link abaixo: <br>
+    {{ route('associates.password.index', $token) }}.</p>
+
+    <p>Este link para refazer sua senha vai expirar em {{ config('auth.password.expire') }} minutos.</p>
+    <p>Caso vocë não tenha solicitado para refazer sua senha, ignore este e-mail.</p>
+
+    <p>Qualquer dúvida <a href="{{ route('contact.index') }}">entre em contato</a> conosco.</p>
 @stop
