ecbbcf18eea69a00d97b7d5ac7d4e9f11e6d592f
Luis Dalmolin <luis.nh@gmail.com>
2016-04-13 11:23:31 -0300

    Adicionado Honeypot nos aliases

--- config/app.php
+++ config/app.php
@@ -208,6 +208,7 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
+        'Honeypot'  => Msurguy\Honeypot\HoneypotFacade::class,
 
     ],
 
