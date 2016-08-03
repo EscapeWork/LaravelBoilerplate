a82c996b0756f9f3fa7becdfa4ee3e0af9babef2
Luis Dalmolin <luis.nh@gmail.com>
2016-08-03 10:40:33 -0300

    Changed default comment on routes.php file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,12 +1,9 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Descrição do site
+| Desenvolvido por: Agência Escape <www.escape.ppg.br>
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
 */
 
 Route::group(['namespace' => 'App', 'middleware' => ['web']], function() {
