fca717fe66473bb891a418c01a1392cd2999e91c
Luis Dalmolin <luis.nh@gmail.com>
2016-08-03 10:38:38 -0300

    Added examples

---
+++ resources/views/manager/menu.blade.php
@@ -0,0 +1,19 @@
+{{-- exemplos
+<li class="{{ $page == 'banners' ? 'active' : null }}">
+    <a href="{{ route('manager.banners.index') }}">Banners</a>
+</li>
+
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        Produtos <span class="caret"></span>
+    </a>
+
+    <ul class="dropdown-menu" role="menu">
+        <li>
+            <a tabindex="-1" href="{{ route('manager.products.categories.index') }}">
+                Categorias
+            </a>
+        </li>
+    </ul>
+</li>
+--}}
