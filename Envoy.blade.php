0c5021b45e94cba6f1802fbdb80bd8c5ceb9087d
Luis Dalmolin <luis.nh@gmail.com>
2016-09-05 17:39:53 -0300

    Added envoy base file

---
+++ Envoy.blade.php
@@ -0,0 +1,28 @@
+@servers(['locaweb' => 'user@server.com.br'])
+
+@task('configure-git-repo')
+    cd ~/
+
+    @if (! is_dir('repos'))
+        mkdir repos
+    @endif
+
+    cd repos
+
+    @if (isset($project))
+        @if (! is_dir($project . '.git'))
+            mkdir {{ $project }}.git
+        @endif
+    @endif
+
+    cd {{ $project }}.git
+    git init --bare
+    cd hooks
+    curl https://gist.githubusercontent.com/luisdalmolin/66cae887fe655491c5cad413d544f434/raw/994a021503dd8c626c4792f51f6609803ae26355/post-receive.sh -o post-receive
+    chmod +x post-receive
+@endtask
+
+@task('deploy')
+    git push origin master
+    git push deploy master
+@endtask
