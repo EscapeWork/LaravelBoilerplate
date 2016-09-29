f632f136592b8e0c9375eb01493d9b827f9bad3f
Luis Dalmolin <luis.nh@gmail.com>
2016-09-29 15:11:59 -0300

    Added new tasks on envoyer

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,6 @@
-@servers(['locaweb' => 'user@server.com'])
+@servers(['locaweb' => 'gabrieladawson1@gabrieladawson1.hospedagemdesites.ws', 'localhost' => '127.0.0.1'])
 
-@task('configure-php')
+@task('configure-php', ['on' => 'locaweb'])
     {{-- http://wiki.locaweb.com.br/pt-br/Como_alterar_a_vers%C3%A3o_do_PHP --}}
     cd ~/
     sed -i -- 's/\/usr\/lib\/php\/modules/\/usr\/lib64\/php56\/modules/g' php.ini
@@ -8,22 +8,11 @@
     sed -i -- 's/register_long_arrays = On/register_long_arrays = Off/g' php.ini
 @endtask
 
-@task('configure-composer')
-    {{-- http://ajuda.locaweb.com.br/pt-br/Composer --}}
-    cd ~/
-    sed -i -- 's/allow_url_fopen = Off/allow_url_fopen = On/g' php.ini
-    echo 'suhosin.executor.include.whitelist = phar' >> php.ini
-    @if (isset($project))
-        cd ~/apps/{{ $project }}
-        curl -sS https://getcomposer.org/installer | php56 -c ~/php.ini
-    @endif
-@endtask
-
-@task('generate-ssh-key')
+@task('generate-ssh-key', ['on' => 'locaweb'])
     ssh-keygen -t rsa
 @endtask
 
-@task('configure-app-directory')
+@task('configure-app-directory', ['on' => 'locaweb'])
     cd ~/
 
     @if (! is_dir('apps'))
@@ -37,7 +26,18 @@
     @endif
 @endtask
 
-@task('configure-git-repo')
+@task('configure-composer', ['on' => 'locaweb'])
+    {{-- http://ajuda.locaweb.com.br/pt-br/Composer --}}
+    cd ~/
+    sed -i -- 's/allow_url_fopen = Off/allow_url_fopen = On/g' php.ini
+    echo 'suhosin.executor.include.whitelist = phar' >> php.ini
+    @if (isset($project))
+        cd ~/apps/{{ $project }}
+        curl -sS https://getcomposer.org/installer | php56 -c ~/php.ini
+    @endif
+@endtask
+
+@task('configure-git-repo', ['on' => 'locaweb'])
     cd ~/
 
     @if (! is_dir('repos'))
@@ -57,4 +57,27 @@
         curl https://gist.githubusercontent.com/luisdalmolin/66cae887fe655491c5cad413d544f434/raw/994a021503dd8c626c4792f51f6609803ae26355/post-receive.sh -o post-receive
         chmod +x post-receive
     @endif
+@endtask
+
+@story('remove-htaccess')
+    remove-htaccess-locally
+    configure-htaccess-remotly
+@endstory
+
+@task('remove-htaccess-locally', ['on' => 'localhost'])
+    echo 'public/.htaccess' >> .gitignore
+    cp public/.htaccess public/htaccess
+    git rm --cached public/.htaccess
+    echo 'Agora commite suas alterações e envie para produção'
+@endtask
+
+@task('configure-htaccess-remotly', ['on' => 'locaweb'])
+    cd ~/
+
+    cd apps/{{ $project }}/public
+    cp htaccess .htaccess
+    echo '' > .htaccess
+    echo '# Locaweb PHP configuration' >> .htaccess
+    echo 'AddHandler php56-script .php' >> .htaccess
+    echo 'suPHP_ConfigPath /home/{{ $ftpuser }}/' >> .htaccess
 @endtask
