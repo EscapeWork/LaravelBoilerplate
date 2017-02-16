bcd32a1f8de8cf43c6af507b4c548dc153069ef3
Luis Dalmolin <luis.nh@gmail.com>
2017-02-16 13:14:50 -0200

    Added a custom database setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-}
+    use CreatesApplication, DatabaseSetup;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setupDatabase();
+    }
