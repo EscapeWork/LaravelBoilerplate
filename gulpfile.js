d2f1eeda592e4e3911894e875be0638a884eb502
Luis Dalmolin <luis.nh@gmail.com>
2017-03-06 17:53:33 -0300

    Removed laravel-elixir, added laravel-mix

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,11 @@
 const gulp        = require('gulp'),
-      elixir      = require('laravel-elixir')
       gutil       = require('gulp-util'),
       uglify      = require('gulp-uglify'),
       favicons    = require('gulp-favicons'),
-      imagemin    = require('gulp-imagemin'),
-      spritesmith = require('gulp.spritesmith');
-
-require('laravel-elixir-vue');
-require('laravel-elixir-livereload');
-
-elixir.config.sourcemaps = false;
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(mix => {
-    mix.sass('main.scss', 'public/assets/css/main.css')
-       //.sass('sections/home.scss', 'public/assets/css/sections/home.scss')
-       .webpack('app/main.js', 'public/assets/js/app/main.min.js');
-
-    mix.livereload();
-});
+      shell       = require('gulp-shell');
 
 gulp.task('imagemin', function() {
-    gulp.src('public/assets/images/**/*')
+    gulp.src('public/assets/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('public/assets/images'));
 });
@@ -48,12 +22,14 @@ gulp.task('favicons', function() {
         .pipe(gulp.dest('public/assets/images/icons'));
 });
 
-gulp.task('sprites', function() {
-    gulp.src('public/assets/images/sprites/default/*.png')
-        .pipe(spritesmith({
-            imgOutput: 'public/assets/images/sprites',
-            imgPath: '/assets/images/sprites/sprite.png',
-            cssName: '../../../../resources/assets/sass/components/_sprite.scss',
-        }))
-        .pipe(gulp.dest('public/assets/images/sprites'));
-});
+gulp.task('default', shell.task([
+    'npm run dev'
+]));
+
+gulp.task('watch', shell.task([
+    'npm run watch'
+]));
+
+gulp.task('production', shell.task([
+    'npm run production'
+]));
